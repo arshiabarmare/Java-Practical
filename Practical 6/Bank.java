6899f90b7851da7d6a62b2ccb527cc7e32efc658
arshiabarmare <arshiabarmare7@gmail.com>
2026-09-25 20:54:26 +0530

    Update Bank.java

--- Practical 6/Bank.java
+++ Practical 6/Bank.java
@@ -1,18 +1,26 @@
 class Account{
-	int bal=60000;
+	double bal=60000;
+
+	void deposit(double damt){
+		bal+=damt;
+	}
+
+	void withdraw(double wamt){
+		bal-=wamt;
+	}
 }
 
-class Deposit extends Thread{
+class deposit extends Thread{
 	Account a;
 
-	Deposit(Account a){
+	deposit(Account a){
 		this.a=a;
 	}
 
 	public void run(){
-		a.bal+=5000;
-		System.out.println("Deposited successfully.");
-		System.out.println("balance:"+ a.bal);
+		System.out.println("Dposite priority: "+ getPriority());
+		a.deposit(5000);
+		System.out.println("Bal after deposit: "+ a.bal);
 	}
 }
 
@@ -24,77 +32,23 @@ class withdraw extends Thread{
 	}
 
 	public void run(){
-		a.bal-=2000;
-		System.out.println("Withdrawed successfully.");
-		System.out.println("balance:"+ a.bal);
+		System.out.println("Withdraw priority: "+ getPriority());
+		a.withdraw(6000);
+		System.out.println("Bal after withdraw: "+ a.bal);
 	}
 }
 
-class Bank{
+
+class bankaccApplication{
 	public static void main(String cp[]){
 		Account a=new Account();
-
-		Deposit d=new Deposit(a);
+		deposit d=new deposit(a);
 		withdraw w=new withdraw(a);
-		
-		d.setPriority(Thread.MAX_PRIORITY);
-		w.setPriority(Thread.MIN_PRIORITY);
 
 		d.start();
+		d.setPriority(Thread.MAX_PRIORITY);
 		w.start();
+		w.setPriority(Thread.MIN_PRIORITY);
 	}
 }
 
-
-
-class Account{
-    int bal=60000;
-
-    synchronized void deposit(){
-        bal+=5000;
-        System.out.println("Deposited successfully.");
-        System.out.println("balance: "+bal);
-    }
-
-    synchronized void withdraw(){
-        bal-=2000;
-        System.out.println("Withdrawn successfully.");
-        System.out.println("balance: "+bal);
-    }
-}
-
-class Deposit extends Thread{
-    Account a;
-
-    Deposit(Account a){
-        this.a=a;
-    }
-
-    public void run(){
-        a.deposit();
-    }
-}
-
-class withdraw extends Thread{
-    Account a;
-
-    withdraw(Account a){
-        this.a=a;
-    }
-
-    public void run(){
-        a.withdraw();
-    }
-}
-
-class Bank{
-    public static void main(String cp[]){
-        Account a=new Account();
-
-        Deposit d=new Deposit(a);
-        withdraw w=new withdraw(a);
-
-        d.start();
-        w.start();
-    }
-}
